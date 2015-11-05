dfede98ba0204368773a665028bf01e237c11a62
SG5 <sergeyv89@gmail.com>
2015-11-05 20:39:52 +0300

    use promises from bluebird

--- server/youtube.js
+++ server/youtube.js
@@ -1,27 +1,25 @@
 var http = require('http');
-var q = require('q');
+var Promise = require('bluebird');
 var parseString = require('xml2js').parseString;
 
 var youtube = {};
 
 youtube.getAnnotations = function(videoId) {
-    var deferred = q.defer();
-
-    http.get('http://www.youtube.com/annotations_invideo?cap_hist=1&video_id='+videoId, function(res) {
-        var str = '';
-        res.on('data', function(chunk) {
-            str += chunk;
-        });
-        res.on('end', function() {
-            parseString(str, function (err, result) {
-                deferred.resolve(result);
+    return new Promise(function(resolve, reject) {
+        http.get('http://www.youtube.com/annotations_invideo?cap_hist=1&video_id='+videoId, function(res) {
+            var str = '';
+            res.on('data', function(chunk) {
+                str += chunk;
             });
+            res.on('end', function() {
+                parseString(str, function (err, result) {
+                    resolve(result);
+                });
+            });
+        }).on('error', function(e) {
+            reject(e);
         });
-    }).on('error', function(e) {
-        deferred.reject(e);
     });
-
-    return deferred.promise;
 };
 
 module.exports = youtube;
